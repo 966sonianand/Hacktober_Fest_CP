91344b793f36eaa1d970969538f4bc2f9be7a512
966sonianand <sonianand966@gmail.com>
2019-10-20 00:39:10 +0530

    pushing b.cpp

---
+++ b.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**************************************************************/
+
+#define int long long
+#define tie() ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define f(i,from,to) for(int i = from ; i <to ; i++)
+#define mem(a , v) memset(a , v , sizeof a)
+#define all(v) v.begin() , v.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+
+
+const int INF = 1e9 + 7;
+const int sz = 1e5 + 20;
+/***************************************************************/
+
+void solve()
+{
+
+    return;
+}
+
+int32_t main()
+{
+    clock_t st = clock();
+    tie();
+    int t = 1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    clog<<(long double)((clock() - st)/CLOCKS_PER_SEC)<<endl;
+    return 0;
+}
